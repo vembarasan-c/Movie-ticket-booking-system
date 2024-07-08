46494026afb1810d96fe3ba674795f9140f571aa
Vembarasan C <151906529+vembarasan-c@users.noreply.github.com>
2024-07-08 14:18:04 +0530

    Update MovieRepository.java

--- server/movie-backend/movie-backend/src/main/java/com/vembarasan/repository/MovieRepository.java
+++ server/movie-backend/movie-backend/src/main/java/com/vembarasan/repository/MovieRepository.java
@@ -4,5 +4,5 @@ import com.vembarasan.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
+  Movie findByTitle(String movieName);
 }
